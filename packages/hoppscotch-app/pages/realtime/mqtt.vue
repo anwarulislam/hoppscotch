a26abcca6bd9f96f6516d887ae213db6730667a7
Anwarul Islam <anwaarulislaam@gmail.com>
2022-05-31 16:58:59 +0600

    feat: authorization tab added

--- packages/hoppscotch-app/pages/realtime/mqtt.vue
+++ packages/hoppscotch-app/pages/realtime/mqtt.vue
@@ -32,23 +32,151 @@
             @click.native="toggleConnection"
           />
         </div>
-        <div class="flex space-x-4">
-          <input
-            id="mqtt-username"
-            v-model="username"
-            type="text"
-            spellcheck="false"
-            class="input"
-            :placeholder="$t('authorization.username')"
+      </div>
+
+      <div>
+        <div
+          class="sticky z-10 flex items-center justify-between pl-4 border-b bg-primary border-dividerLight top-upperPrimaryStickyFold"
+        >
+          <span class="flex items-center">
+            <label class="font-semibold text-secondaryLight">
+              {{ t("authorization.type") }}
+            </label>
+            <tippy
+              ref="authTypeOptions"
+              interactive
+              trigger="click"
+              theme="popover"
+              arrow
+            >
+              <template #trigger>
+                <span class="select-wrapper">
+                  <ButtonSecondary
+                    class="pr-8 ml-2 rounded-none"
+                    :label="authType"
+                  />
+                </span>
+              </template>
+              <div class="flex flex-col" role="menu">
+                <SmartItem
+                  label="None"
+                  :icon="
+                    authType === 'none'
+                      ? 'radio_button_checked'
+                      : 'radio_button_unchecked'
+                  "
+                  :active="authType === 'none'"
+                  @click.native="
+                    () => {
+                      authType = 'none'
+                      authTypeOptions.tippy().hide()
+                    }
+                  "
+                />
+                <SmartItem
+                  label="Basic Auth"
+                  :icon="
+                    authType === 'basic'
+                      ? 'radio_button_checked'
+                      : 'radio_button_unchecked'
+                  "
+                  :active="authType === 'basic'"
+                  @click.native="
+                    () => {
+                      authType = 'basic'
+                      authTypeOptions.tippy().hide()
+                    }
+                  "
+                />
+                <SmartItem
+                  label="Token"
+                  :icon="
+                    authType === 'token'
+                      ? 'radio_button_checked'
+                      : 'radio_button_unchecked'
+                  "
+                  :active="authType === 'token'"
+                  @click.native="
+                    () => {
+                      authType = 'token'
+                      authTypeOptions.tippy().hide()
+                    }
+                  "
+                />
+              </div>
+            </tippy>
+          </span>
+          <div class="flex">
+            <ButtonSecondary
+              v-tippy="{ theme: 'tooltip' }"
+              to="https://docs.hoppscotch.io/features/authorization"
+              blank
+              :title="t('app.wiki')"
+              svg="help-circle"
+            />
+          </div>
+        </div>
+        <div
+          v-if="authType === 'none'"
+          class="flex flex-col items-center justify-center p-4 text-secondaryLight"
+        >
+          <img
+            :src="`/images/states/${$colorMode.value}/login.svg`"
+            loading="lazy"
+            class="inline-flex flex-col object-contain object-center w-16 h-16 my-4"
+            :alt="`${t('empty.authorization')}`"
           />
-          <input
-            id="mqtt-password"
-            v-model="password"
-            type="password"
-            spellcheck="false"
-            class="input"
-            :placeholder="$t('authorization.password')"
+          <span class="pb-4 text-center">
+            {{ t("mqtt.connection_not_authorized") }}
+          </span>
+          <ButtonSecondary
+            outline
+            :label="t('app.documentation')"
+            to="https://docs.hoppscotch.io/features/authorization"
+            blank
+            svg="external-link"
+            reverse
+            class="mb-4"
           />
+        </div>
+        <div v-if="authType === 'basic'">
+          <div class="flex flex-1 border-b border-dividerLight">
+            <SmartEnvInput
+              v-model="username"
+              :placeholder="$t('authorization.username')"
+            />
+          </div>
+          <div class="flex flex-1 border-b border-dividerLight">
+            <SmartEnvInput
+              v-model="password"
+              :placeholder="$t('authorization.password')"
+            />
+          </div>
+        </div>
+        <div
+          v-if="authType === 'token'"
+          class="flex flex-1 border-b border-dividerLight"
+        >
+          <div class="w-2/3 border-r border-dividerLight">
+            <div class="flex flex-1 border-b border-dividerLight">
+              <SmartEnvInput v-model="token" placeholder="Token" />
+            </div>
+          </div>
+          <div
+            class="sticky h-full p-4 overflow-auto bg-primary top-upperTertiaryStickyFold min-w-46 max-w-1/3 z-9"
+          >
+            <div class="p-2">
+              <div class="pb-2 text-secondaryLight">
+                {{ t("helpers.authorization") }}
+              </div>
+              <SmartAnchor
+                class="link"
+                :label="`${t('authorization.learn')} \xA0 →`"
+                to="https://docs.hoppscotch.io/features/authorization"
+                blank
+              />
+            </div>
+          </div>
         </div>
       </div>
     </template>
@@ -180,8 +308,17 @@ const isUrlValid = ref(true)
 const pubTopic = ref("")
 const subTopic = ref("")
 const message = ref("")
+
+// Authorization
+const authTypeOptions = ref<HTMLElement>()
+const authType = ref<"none" | "basic" | "token">("none")
+
+// Basic Auth
 const username = ref("")
 const password = ref("")
+
+// Token Auth
+const token = ref("")
 
 let worker: Worker
 
