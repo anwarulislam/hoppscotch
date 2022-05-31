b573b21100fa61f1916ccb996e7a673eb96be239
Anwarul Islam <anwaarulislaam@gmail.com>
2022-05-31 17:36:07 +0600

    feat: communicatoin tab added

--- packages/hoppscotch-app/pages/realtime/mqtt.vue
+++ packages/hoppscotch-app/pages/realtime/mqtt.vue
@@ -34,151 +34,166 @@
         </div>
       </div>
 
-      <div>
-        <div
-          class="sticky z-10 flex items-center justify-between pl-4 border-b bg-primary border-dividerLight top-upperPrimaryStickyFold"
+      <SmartTabs
+        v-model="selectedTab"
+        styles="sticky bg-primary top-upperMobilePrimaryStickyFold sm:top-upperPrimaryStickyFold z-10"
+      >
+        <SmartTab
+          :id="'communication'"
+          :label="`${t('websocket.communication')}`"
         >
-          <span class="flex items-center">
-            <label class="font-semibold text-secondaryLight">
-              {{ t("authorization.type") }}
-            </label>
-            <tippy
-              ref="authTypeOptions"
-              interactive
-              trigger="click"
-              theme="popover"
-              arrow
-            >
-              <template #trigger>
-                <span class="select-wrapper">
-                  <ButtonSecondary
-                    class="pr-8 ml-2 rounded-none"
-                    :label="authType"
-                  />
-                </span>
-              </template>
-              <div class="flex flex-col" role="menu">
-                <SmartItem
-                  label="None"
-                  :icon="
-                    authType === 'none'
-                      ? 'radio_button_checked'
-                      : 'radio_button_unchecked'
-                  "
-                  :active="authType === 'none'"
-                  @click.native="
-                    () => {
-                      authType = 'none'
-                      authTypeOptions.tippy().hide()
-                    }
-                  "
-                />
-                <SmartItem
-                  label="Basic Auth"
-                  :icon="
-                    authType === 'basic'
-                      ? 'radio_button_checked'
-                      : 'radio_button_unchecked'
-                  "
-                  :active="authType === 'basic'"
-                  @click.native="
-                    () => {
-                      authType = 'basic'
-                      authTypeOptions.tippy().hide()
-                    }
-                  "
-                />
-                <SmartItem
-                  label="Token"
-                  :icon="
-                    authType === 'token'
-                      ? 'radio_button_checked'
-                      : 'radio_button_unchecked'
-                  "
-                  :active="authType === 'token'"
-                  @click.native="
-                    () => {
-                      authType = 'token'
-                      authTypeOptions.tippy().hide()
-                    }
-                  "
-                />
-              </div>
-            </tippy>
-          </span>
-          <div class="flex">
-            <ButtonSecondary
-              v-tippy="{ theme: 'tooltip' }"
-              to="https://docs.hoppscotch.io/features/authorization"
-              blank
-              :title="t('app.wiki')"
-              svg="help-circle"
-            />
-          </div>
-        </div>
-        <div
-          v-if="authType === 'none'"
-          class="flex flex-col items-center justify-center p-4 text-secondaryLight"
-        >
-          <img
-            :src="`/images/states/${$colorMode.value}/login.svg`"
-            loading="lazy"
-            class="inline-flex flex-col object-contain object-center w-16 h-16 my-4"
-            :alt="`${t('empty.authorization')}`"
-          />
-          <span class="pb-4 text-center">
-            {{ t("mqtt.connection_not_authorized") }}
-          </span>
-          <ButtonSecondary
-            outline
-            :label="t('app.documentation')"
-            to="https://docs.hoppscotch.io/features/authorization"
-            blank
-            svg="external-link"
-            reverse
-            class="mb-4"
-          />
-        </div>
-
-        <div v-else class="flex flex-1 border-b border-dividerLight">
-          <div class="w-2/3 border-r border-dividerLight">
-            <div v-if="authType === 'basic'">
-              <div class="flex flex-1 border-b border-dividerLight">
-                <SmartEnvInput
-                  v-model="username"
-                  :placeholder="$t('authorization.username')"
-                />
-              </div>
-              <div class="flex flex-1 border-b border-dividerLight">
-                <SmartEnvInput
-                  v-model="password"
-                  :placeholder="$t('authorization.password')"
-                />
-              </div>
-            </div>
-            <div v-if="authType === 'token'">
-              <div class="flex flex-1 border-b border-dividerLight">
-                <SmartEnvInput v-model="token" placeholder="Token" />
-              </div>
-            </div>
-          </div>
+          <RealtimeCommunication
+            :show-event-field="true"
+            :is-connected="connectionState === 'CONNECTED'"
+            @send-message="publish($event)"
+          ></RealtimeCommunication>
+        </SmartTab>
+        <SmartTab :id="'protocols'" :label="`${t('request.authorization')}`">
           <div
-            class="sticky h-full p-4 overflow-auto bg-primary top-upperTertiaryStickyFold min-w-46 max-w-1/3 z-9"
+            class="sticky z-10 flex items-center justify-between pl-4 border-b bg-primary border-dividerLight top-upperPrimaryStickyFold"
           >
-            <div class="p-2">
-              <div class="pb-2 text-secondaryLight">
-                {{ t("helpers.authorization") }}
-              </div>
-              <SmartAnchor
-                class="link"
-                :label="`${t('authorization.learn')} \xA0 →`"
+            <span class="flex items-center">
+              <label class="font-semibold text-secondaryLight">
+                {{ t("authorization.type") }}
+              </label>
+              <tippy
+                ref="authTypeOptions"
+                interactive
+                trigger="click"
+                theme="popover"
+                arrow
+              >
+                <template #trigger>
+                  <span class="select-wrapper">
+                    <ButtonSecondary
+                      class="pr-8 ml-2 rounded-none"
+                      :label="authType"
+                    />
+                  </span>
+                </template>
+                <div class="flex flex-col" role="menu">
+                  <SmartItem
+                    label="None"
+                    :icon="
+                      authType === 'none'
+                        ? 'radio_button_checked'
+                        : 'radio_button_unchecked'
+                    "
+                    :active="authType === 'none'"
+                    @click.native="
+                      () => {
+                        authType = 'none'
+                        authTypeOptions.tippy().hide()
+                      }
+                    "
+                  />
+                  <SmartItem
+                    label="Basic Auth"
+                    :icon="
+                      authType === 'basic'
+                        ? 'radio_button_checked'
+                        : 'radio_button_unchecked'
+                    "
+                    :active="authType === 'basic'"
+                    @click.native="
+                      () => {
+                        authType = 'basic'
+                        authTypeOptions.tippy().hide()
+                      }
+                    "
+                  />
+                  <SmartItem
+                    label="Token"
+                    :icon="
+                      authType === 'token'
+                        ? 'radio_button_checked'
+                        : 'radio_button_unchecked'
+                    "
+                    :active="authType === 'token'"
+                    @click.native="
+                      () => {
+                        authType = 'token'
+                        authTypeOptions.tippy().hide()
+                      }
+                    "
+                  />
+                </div>
+              </tippy>
+            </span>
+            <div class="flex">
+              <ButtonSecondary
+                v-tippy="{ theme: 'tooltip' }"
                 to="https://docs.hoppscotch.io/features/authorization"
                 blank
+                :title="t('app.wiki')"
+                svg="help-circle"
               />
             </div>
           </div>
-        </div>
-      </div>
+          <div
+            v-if="authType === 'none'"
+            class="flex flex-col items-center justify-center p-4 text-secondaryLight"
+          >
+            <img
+              :src="`/images/states/${$colorMode.value}/login.svg`"
+              loading="lazy"
+              class="inline-flex flex-col object-contain object-center w-16 h-16 my-4"
+              :alt="`${t('empty.authorization')}`"
+            />
+            <span class="pb-4 text-center">
+              {{ t("mqtt.connection_not_authorized") }}
+            </span>
+            <ButtonSecondary
+              outline
+              :label="t('app.documentation')"
+              to="https://docs.hoppscotch.io/features/authorization"
+              blank
+              svg="external-link"
+              reverse
+              class="mb-4"
+            />
+          </div>
+
+          <div v-else class="flex flex-1 border-b border-dividerLight">
+            <div class="w-2/3 border-r border-dividerLight">
+              <div v-if="authType === 'basic'">
+                <div class="flex flex-1 border-b border-dividerLight">
+                  <SmartEnvInput
+                    v-model="username"
+                    :placeholder="$t('authorization.username')"
+                  />
+                </div>
+                <div class="flex flex-1 border-b border-dividerLight">
+                  <SmartEnvInput
+                    v-model="password"
+                    :placeholder="$t('authorization.password')"
+                  />
+                </div>
+              </div>
+              <div v-if="authType === 'token'">
+                <div class="flex flex-1 border-b border-dividerLight">
+                  <SmartEnvInput v-model="token" placeholder="Token" />
+                </div>
+              </div>
+            </div>
+            <div
+              class="sticky h-full p-4 overflow-auto bg-primary top-upperTertiaryStickyFold min-w-46 max-w-1/3 z-9"
+            >
+              <div class="p-2">
+                <div class="pb-2 text-secondaryLight">
+                  {{ t("helpers.authorization") }}
+                </div>
+                <SmartAnchor
+                  class="link"
+                  :label="`${t('authorization.learn')} \xA0 →`"
+                  to="https://docs.hoppscotch.io/features/authorization"
+                  blank
+                />
+              </div>
+            </div>
+          </div>
+        </SmartTab>
+      </SmartTabs>
     </template>
     <template #secondary>
       <RealtimeLog
@@ -189,47 +204,6 @@
     </template>
     <template #sidebar>
       <div class="flex items-center justify-between p-4">
-        <label for="pubTopic" class="font-semibold text-secondaryLight">
-          {{ $t("mqtt.topic") }}
-        </label>
-      </div>
-      <div class="flex px-4">
-        <input
-          id="pubTopic"
-          v-model="pubTopic"
-          class="input"
-          :placeholder="$t('mqtt.topic_name')"
-          type="text"
-          autocomplete="off"
-          spellcheck="false"
-        />
-      </div>
-      <div class="flex items-center justify-between p-4">
-        <label for="mqtt-message" class="font-semibold text-secondaryLight">
-          {{ $t("mqtt.communication") }}
-        </label>
-      </div>
-      <div class="flex px-4 space-x-2">
-        <input
-          id="mqtt-message"
-          v-model="message"
-          class="input"
-          type="text"
-          autocomplete="off"
-          :placeholder="$t('mqtt.message')"
-          spellcheck="false"
-        />
-        <ButtonPrimary
-          id="publish"
-          name="get"
-          :disabled="!canPublish"
-          :label="$t('mqtt.publish')"
-          @click.native="publish"
-        />
-      </div>
-      <div
-        class="flex items-center justify-between p-4 mt-4 border-t border-dividerLight"
-      >
         <label for="subTopic" class="font-semibold text-secondaryLight">
           {{ $t("mqtt.topic") }}
         </label>
@@ -304,10 +278,11 @@ const subscriptionState = useReadonlyStream(
   false
 )
 
+type MQTTTab = "communication" | "authorization"
+const selectedTab = ref<MQTTTab>("communication")
+
 const isUrlValid = ref(true)
-const pubTopic = ref("")
 const subTopic = ref("")
-const message = ref("")
 
 // Authorization
 const authTypeOptions = ref<HTMLElement>()
@@ -322,12 +297,6 @@ const token = ref("")
 
 let worker: Worker
 
-const canPublish = computed(
-  () =>
-    pubTopic.value !== "" &&
-    message.value !== "" &&
-    connectionState.value === "CONNECTED"
-)
 const canSubscribe = computed(
   () => subTopic.value !== "" && connectionState.value === "CONNECTED"
 )
@@ -450,8 +419,8 @@ const toggleConnection = () => {
   // Otherwise, it's disconnecting.
   socket.value.disconnect()
 }
-const publish = () => {
-  socket.value?.publish(pubTopic.value, message.value)
+const publish = (event: { message: string; eventName: string }) => {
+  socket.value?.publish(event.eventName, event.message)
 }
 const toggleSubscription = () => {
   if (subscriptionState.value) {
